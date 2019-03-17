70930930e0262669455dad8de837c4b92a410cbc
FlakMeister <sukrutakolkar@gmail.com>
2019-03-18 00:15:57 +0530

    day2 v2

--- day2/SortBySecondCharacter.py
+++ day2/SortBySecondCharacter.py
@@ -1,1 +1,15 @@
-lst = []
+def SortBySecondCharacter(d,lst):	
+	for i in range(0,len(lst)):
+			if((lst[i])[1] == d[1]):
+				lst.insert(i,d)
+				return
+
+			if(i==len(lst)-1):
+				lst.append(d)
+				return
+
+
+lst = ["cat","bat","rat","abc","cbd","acc","bcc"]
+
+SortBySecondCharacter("ced",lst)
+print(lst)
